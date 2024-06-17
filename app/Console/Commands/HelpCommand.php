b6f324c82878fc2d290080be0ac52722291467d2
catalagil.ismael <catalagil.ismael@gmail.com>
2024-06-17 08:23:00 +0000

    primer commit

---
+++ app/Console/Commands/HelpCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class HelpCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:help-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        
+    }
+}
