7a2fba3f07bdaa8cf4de1c482aefc1ba411d9e54
catalagil.ismael <catalagil.ismael@gmail.com>
2024-06-19 16:03:18 +0000

    laravel breeze

--- routes/task.php
+++ routes/task.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/task', function () {
     return "task desde task.php";
 });
+Route::get('/hello', function () {
+    return "HOLA EL RINCON DE ISMA";
+});
